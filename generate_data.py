9db45eec83cf8205c3e482c6228e40bd87dda90e
Ranith Sachintha Ranawaka <ranithsachin@LINKIT-MB0312.local>
2024-04-21 23:15:51 +0200

    adding items generation

--- generate_data.py
+++ generate_data.py
@@ -5,6 +5,7 @@ from pyspark.sql import Row
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType,FloatType,TimestampType
 import random
 import datetime as dt
+import pandas as pd
 
 def customer_id_generator(i):
     alpha_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'F',7:'G',8:'H',9:'I'}
@@ -46,19 +47,13 @@ def generate_customer_data():
 def generate_items_data():
     fake = Faker()
 
-    items_schema = StructType([
-        StructField("ITEM_ID", IntegerType()),
-        StructField("ITEM_EAN", StringType()),
-        StructField("ITEM_DEPARTMENT", StringType()),
-        StructField("ITEM_VALUE", FloatType())]
-    )
-
-    items_df = spark.createDataFrame([],items_schema)
-
     departments = []
     for i in range(50):
         Faker.seed(i)
         departments.append(fake.word())
+
+    Faker.seed()
+    items = []
 
     for i in range(300):
         Faker.seed(i)
@@ -67,11 +62,57 @@ def generate_items_data():
         item_dept = fake.word(ext_word_list=departments)
         item_id = fake.unique.random_int(0,300)
 
-        item_row = spark.createDataFrame([(item_id,item_ean,item_dept,item_value)], items_schema)
+        item_row = {"ITEM_ID": item_id,"ITEM_EAN":item_ean,"ITEM_DEPARTMENT":item_dept,"ITEM_VALUE":item_value}
 
-        items_df = items_df.union(item_row)
+        items.append(item_row)
+        Faker.seed()
 
-    Faker.seed()
+    items_df = pd.DataFrame(items)
+    return items_df
+
+def generate_transactions_data():
+    fake = Faker()
+
+    creditcard_transaction_schema = StructType([StructField("TRANSACTION_ID", StringType()),
+                                    StructField("CUSTOMER_NUMBER", StringType()),
+                                    StructField("TRANSACTION_VALUE", FloatType()),
+                                    StructField("TRANSACTION_DATE_TIME", TimestampType()),
+                                    StructField("NUMBER_OF_ITEMS", IntegerType())])
+
+    product_transaction_schema =    StructType([StructField("TRANSACTION_ID", StringType()),
+                                    StructField("ITEM_EAN", StringType()),
+                                    StructField("ITEM_DEPARTMENT", StringType()),
+                                    StructField("ITEM_VALUE", FloatType()),
+                                    StructField("ITEM_ID", StringType()),
+                                    StructField("ITEM_QUANTITY", IntegerType())])
+    
+    creditcard_df = spark.createDataFrame([],creditcard_transaction_schema)
+    product_df = spark.createDataFrame([],product_transaction_schema)
+
+    customer_number_df = spark.read.table("CUSTOMER_DATA").select("CUSTOMER_NUMBER")
+    customer_number_list = [row.CUSTOMER_NUMBER for row in customer_number_df.select('CUSTOMER_NUMBER').collect()]
+
+    items_df = generate_items_data()
+
+    for i in range(100):
+        trans_id = fake.unique.random_int(min=20000, max=40000)
+        item = items_df.sample()
+        item_quantity = fake.random_int(1,200)
+        trans_value = float((item.get('ITEM_VALUE').values[0])*item_quantity)
+
+        trans_date_time = fake.date_time_between(dt.date(2022,1,1), dt.date(2022,12,31))
+
+        rand_idx = random.randrange(len(customer_number_list))
+        cust_number = customer_number_list[rand_idx]
+
+        creditcard_row = spark.createDataFrame([(trans_id,cust_number,trans_value,trans_date_time,item_quantity)],creditcard_transaction_schema)
+        creditcard_df = creditcard_df.union(creditcard_row)
+
+        product_row = spark.createDataFrame([(trans_id,(item.get('ITEM_EAN').values[0]),(item.get('ITEM_DEPARTMENT').values[0]),float(item.get('ITEM_VALUE').values[0]),(item.get('ITEM_ID').values[0]),item_quantity)],product_transaction_schema)
+        product_df = product_df.union(product_row)
+
+    creditcard_df.write.mode('append').saveAsTable('CREDITCARD_TRANSACTION')
+    product_df.write.mode('append').saveAsTable('PRODUCT_TRANSACTION')
 
 #generate_customer_data()
 generate_items_data()
